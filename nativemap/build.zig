d66455ba7608848fca8348cd128160e2298ed342
tsunko <tsunko@protonmail.com>
2021-07-17 04:15:01 -0700

    Resolve includes and lib paths based on project root, Tracy integration

--- nativemap/build.zig
+++ nativemap/build.zig
@@ -1,51 +1,101 @@
 const std = @import("std");
+const fs = std.fs;
+
+const Allocator = std.mem.Allocator;
 const Builder = std.build.Builder;
+
+var root: []const u8 = undefined;
+var allocator: *Allocator = undefined;
+
+// set to true if we want tracy to record frame/function times, false if we just want stubbed
+var enable_tracy = true;
 
 pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
-    const debugPlayer = b.addExecutable("sdl-debug-player", "src/sdl-main.zig");
-    debugPlayer.setTarget(target);
-    debugPlayer.setBuildMode(mode);
-    debugPlayer.linkLibC();
-    buildWithSDL2(debugPlayer);
-    buildWithFFmpeg(debugPlayer);
-    debugPlayer.install();
+    root = b.build_root;
+    allocator = b.allocator;
 
-    const lutGen = b.addExecutable("lut-gen", "src/lut-main.zig");
-    lutGen.setTarget(target);
-    lutGen.setBuildMode(mode);
-    lutGen.install();
+    {
+        const debugPlayer = b.addExecutable("sdl-debug-player", "src/sdl-main.zig");
+        debugPlayer.setTarget(target);
+        debugPlayer.setBuildMode(mode);
+        debugPlayer.linkLibC();
+        debugPlayer.want_lto = false;
+        buildWithSDL2(debugPlayer);
+        buildWithFFmpeg(debugPlayer);
+        buildWithTracy(debugPlayer);
 
-    const jniLib = b.addSharedLibrary("nativemap", "src/lib.zig", b.version(0, 0, 1));
-    jniLib.setTarget(target);
-    jniLib.setBuildMode(mode);
-    jniLib.linkLibC();
-    buildWithJNI(jniLib);
-    buildWithFFmpeg(jniLib);
-    jniLib.install();
+        debugPlayer.install();
+
+        const runDebugPlayer = debugPlayer.run();
+        runDebugPlayer.cwd = b.exe_dir;
+
+        const runDebugStep = b.step("debug", "Run the debug player");
+        runDebugStep.dependOn(&runDebugPlayer.step);
+    }
+    {
+        const lutGen = b.addExecutable("lut-gen", "src/lut-main.zig");
+        lutGen.setTarget(target);
+        lutGen.setBuildMode(mode);
+        lutGen.install();
+    }
+    {
+        const jniLib = b.addSharedLibrary("nativemap", "src/lib.zig", b.version(0, 0, 1));
+        jniLib.setTarget(target);
+        jniLib.setBuildMode(mode);
+        jniLib.linkLibC();
+        buildWithJNI(jniLib);
+        buildWithFFmpeg(jniLib);
+        buildWithTracy(jniLib);
+        jniLib.install();
+    }
 }
 
 fn buildWithSDL2(target: *std.build.LibExeObjStep) void {
-    target.addIncludeDir("sdl2\\include");
-    target.addLibPath("sdl2\\lib\\x64");
+    target.addIncludeDir(joinWithRoot("\\sdl2\\include"));
+    target.addLibPath(joinWithRoot("\\sdl2\\lib\\x64"));
     target.linkSystemLibrary("sdl2");
 }
 
 fn buildWithFFmpeg(target: *std.build.LibExeObjStep) void {
-    target.addIncludeDir("ffmpeg\\include");
-    target.addLibPath("ffmpeg\\lib");
+    target.addIncludeDir(joinWithRoot("\\ffmpeg\\include"));
+    target.addLibPath(joinWithRoot("\\ffmpeg\\lib"));
     target.linkSystemLibrary("avcodec");
     target.linkSystemLibrary("swresample");
     target.linkSystemLibrary("avutil");
     target.linkSystemLibrary("avformat");
-    target.linkSystemLibrary("avfilter");
-    target.linkSystemLibrary("avdevice");
     target.linkSystemLibrary("swscale");
 }
 
 fn buildWithJNI(target: *std.build.LibExeObjStep) void {
-    target.addIncludeDir("jni\\include");
-    target.addIncludeDir("jni\\include\\win32");
+    target.addIncludeDir(joinWithRoot("\\jni\\include"));
+    target.addIncludeDir(joinWithRoot("\\jni\\include\\win32"));
+}
+
+fn buildWithTracy(target: *std.build.LibExeObjStep) void {
+    const tracyPath = joinWithRoot("\\tracy-0.7.8");
+    target.addBuildOption(bool, "tracy_enabled", enable_tracy);
+    target.addIncludeDir(tracyPath);
+    const tracyClient = std.fs.path.join(allocator, &.{ tracyPath, "TracyClient.cpp" }) catch unreachable;
+    target.addCSourceFile(tracyClient, &.{
+        "-fno-sanitize=undefined",
+        "-DTRACY_ENABLE",
+        "-D_WIN32_WINNT=0x601",
+    });
+
+    // if building from source, make sure you change this to match your specific windows SDK install
+    target.addLibPath("C:\\Program Files (x86)\\Windows Kits\\10\\Lib\\10.0.19041.0\\um\\x64");
+    target.linkSystemLibrary("DbgHelp");
+    target.linkSystemLibrary("Advapi32");
+    target.linkSystemLibrary("User32");
+    target.linkSystemLibrary("Ws2_32");
+
+    target.linkLibC();
+    target.linkSystemLibrary("c++");
+}
+
+fn joinWithRoot(path: []const u8) []const u8 {
+    return fs.path.join(allocator, &.{ root, path }) catch unreachable;
 }
