06af87ae0a87c2553ccaa0428fe1bbc6c00f93f4
tsunko <tsunko@protonmail.com>
2021-07-17 04:16:09 -0700

    A bunch of small changes - Use av_find_best_stream instead of home-made findVideoStream - Change videoIndex to usize for indexing - Remove 'zig fmt' switches in favor of just having 'zig fmt' expand parameters to their own line - Remove initial suspension of _stepNextFrame in favor of having it handled by stepNextFrame - Tracy integration

--- nativemap/src/frame-by-frame.zig
+++ nativemap/src/frame-by-frame.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const tracy = @import("tracy.zig");
 
 usingnamespace @cImport({
     @cInclude("libavcodec/avcodec.h");
@@ -24,14 +25,16 @@ pub fn FrameByFrame(comptime UserDataType: type) type {
         outFrame: [*c]AVFrame = undefined,
         swsContext: *SwsContext = undefined,
 
-        videoIndex: u16 = 0,
+        videoIndex: usize = 0,
         callback: ?fn ([*]const u8, UserDataType) void = null,
         userData: UserDataType = undefined,
         asyncFrame: @Frame(_stepNextFrame) = undefined,
         done: bool = false,
+        started: bool = false,
 
         pub fn setup(self: *Self, source: [*:0]const u8, targetWidth: u16, targetHeight: u16) !f64 {
             self.formatContext = avformat_alloc_context();
+
             self.inFrame = av_frame_alloc();
             self.outFrame = av_frame_alloc();
 
@@ -49,9 +52,15 @@ pub fn FrameByFrame(comptime UserDataType: type) type {
             }
 
             // then filter all streams to find the video stream (we assume it's the first one found)
-            self.videoIndex = findVideoStream(self.formatContext, &self.codecContext, &self.codec);
-            if (self.videoIndex == comptime std.math.maxInt(u16)) {
+            var index = av_find_best_stream(self.formatContext, AVMEDIA_TYPE_VIDEO, -1, -1, &self.codec, 0);
+            if (index < 0) {
                 return AVError.NoStreamFound;
+            }
+            self.videoIndex = @intCast(usize, index);
+
+            self.codecContext = avcodec_alloc_context3(self.codec);
+            if (avcodec_parameters_to_context(self.codecContext, self.formatContext.*.streams[self.videoIndex].*.codecpar) < 0) {
+                return AVError.FailedFindStreamInfo;
             }
 
             // open a codec for decoding
@@ -59,43 +68,47 @@ pub fn FrameByFrame(comptime UserDataType: type) type {
                 return AVError.FailedCodecOpen;
             }
 
-            // zig fmt: off
             // allocate space for our output frame
             const outFrameBufferSize = av_image_get_buffer_size(
-                AV_PIX_FMT_RGB444LE, 
-                targetWidth, targetHeight, 
-                alignment
+                AV_PIX_FMT_RGB444LE,
+                targetWidth,
+                targetHeight,
+                alignment,
             );
             const buffer = av_malloc(@intCast(usize, outFrameBufferSize) * @sizeOf(u8)).?;
-            
+
             // then fill out our outFrame with suitable parameters for calling back to
             _ = av_image_fill_arrays(
-                @ptrCast([*c][*c]u8, &self.outFrame.*.data[0]), 
-                @ptrCast([*c]c_int, &self.outFrame.*.linesize[0]), 
-                @ptrCast([*c]const u8, buffer), 
-                AV_PIX_FMT_RGB444LE, 
-                targetWidth, targetHeight, alignment
+                @ptrCast([*c][*c]u8, &self.outFrame.*.data[0]),
+                @ptrCast([*c]c_int, &self.outFrame.*.linesize[0]),
+                @ptrCast([*c]const u8, buffer),
+                AV_PIX_FMT_RGB444LE,
+                targetWidth,
+                targetHeight,
+                alignment,
             );
 
             // to get this to compile with this, you have to copy and paste the function definition
             // of sws_alloc_set_opts into swscale.h, as swscale_internal.h isn't provided
             self.swsContext = sws_getContext(
                 // source parameters
-                self.codecContext.*.width, self.codecContext.*.height,
+                self.codecContext.*.width,
+                self.codecContext.*.height,
                 self.codecContext.*.pix_fmt,
 
                 // target parameters
-                targetWidth, targetHeight,
+                targetWidth,
+                targetHeight,
                 AV_PIX_FMT_RGB444LE,
 
                 // scaling algorithm - use a crappy scaling algorithm because of performance
                 SWS_POINT,
-                null, null, null
+                null,
+                null,
+                null,
             ) orelse return AVError.FailedSwrContextAlloc;
 
-            self.asyncFrame = async self._stepNextFrame();
             self.done = false;
-            // zig fmt: on
 
             // return frame delay
             return 1000.0 * (1.0 / av_q2d(self.formatContext.*.streams[self.videoIndex].*.r_frame_rate));
@@ -112,15 +125,17 @@ pub fn FrameByFrame(comptime UserDataType: type) type {
         // wrapper function for resuming the internal probably-suspended function
         pub fn stepNextFrame(self: *Self) bool {
             if (self.done) return false;
-            resume self.asyncFrame;
+            if (!self.started) {
+                self.asyncFrame = async self._stepNextFrame();
+                self.started = true;
+            } else {
+                resume self.asyncFrame;
+            }
             return true;
         }
 
         // internally the async function used for grabbing the next frame
         fn _stepNextFrame(self: *Self) void {
-            // initially suspend
-            suspend {}
-
             if (self.callback == null or self.done) {
                 self.done = true;
                 return;
@@ -143,20 +158,23 @@ pub fn FrameByFrame(comptime UserDataType: type) type {
                             return;
                         }
 
-                        // zig fmt: off
+                        const tracy_sws_scale = tracy.ZoneN(@src(), "sws_scale");
                         // more ugly pointer casting
-                        _ = sws_scale(self.swsContext, 
-                            @ptrCast([*c]const [*c]const u8, &self.inFrame.*.data[0]),  
-                            @ptrCast([*c]const c_int, &self.inFrame.*.linesize[0]), 
-                            0, 
-                            self.codecContext.*.height, 
-                            @ptrCast([*c]const [*c]u8, &self.outFrame.*.data[0]), 
-                            @ptrCast([*c]c_int, &self.outFrame.*.linesize[0])
+                        _ = sws_scale(
+                            self.swsContext,
+                            @ptrCast([*c]const [*c]const u8, &self.inFrame.*.data[0]),
+                            @ptrCast([*c]const c_int, &self.inFrame.*.linesize[0]),
+                            0,
+                            self.codecContext.*.height,
+                            @ptrCast([*c]const [*c]u8, &self.outFrame.*.data[0]),
+                            @ptrCast([*c]c_int, &self.outFrame.*.linesize[0]),
                         );
+                        tracy_sws_scale.End();
 
-                        // zig fmt: on
+                        const tracy_callback = tracy.ZoneN(@src(), "callback");
                         // pass the frame and frame delay over to the callback function
                         self.callback.?(self.outFrame.*.data[0], self.userData);
+                        tracy_callback.End();
                         suspend {}
                     }
                 }
@@ -175,31 +193,4 @@ pub fn FrameByFrame(comptime UserDataType: type) type {
             sws_freeContext(self.swsContext);
         }
     };
-}
-
-fn findVideoStream(formatContext: *AVFormatContext, contextOut: *[*c]AVCodecContext, codecOut: *[*c]AVCodec) u16 {
-    var videoIndex: u16 = std.math.maxInt(u16);
-    var i: u16 = 0;
-    while (i < formatContext.*.nb_streams) : (i += 1) {
-        const param: *AVCodecParameters = formatContext.*.streams[i].*.codecpar;
-        const codec: [*c]AVCodec = avcodec_find_decoder(param.*.codec_id);
-        // skip over unsupported codec
-        if (codec == null) continue;
-
-        if (param.*.codec_type == AVMEDIA_TYPE_VIDEO) {
-            codecOut.* = codec;
-            contextOut.* = avcodec_alloc_context3(codec);
-
-            if (avcodec_parameters_to_context(contextOut.*, param) < 0) {
-                // maybe try to find another one?
-                continue;
-            }
-
-            // successfully found something
-            videoIndex = i;
-            break;
-        }
-    }
-
-    return videoIndex;
 }
