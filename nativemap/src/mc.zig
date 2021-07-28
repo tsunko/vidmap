4d533be4cb888806785706c8b379f4ea2c677199
tsunko <tsunko@protonmail.com>
2021-07-27 22:54:50 -0700

    Implement multithreaded color matching

--- nativemap/src/mc.zig
+++ nativemap/src/mc.zig
@@ -1,16 +1,29 @@
-const fbf = @import("frame-by-frame.zig");
-
+const std = @import("std");
 pub const ColorLookupTable: []const u8 align(32) = @embedFile("lut.dat");
 pub const MapByteSize: usize = (128 * 128);
 
+usingnamespace @import("threadpool");
+
+const PartitionPool = ThreadPool(processMapPartition, null);
+var coreCount: usize = undefined;
+var pool: ?*PartitionPool = null;
+
 pub const NativeMapContext = struct {
     buffer: [*]u8,
-    mapWidth: u32,
-    mapHeight: u32,
+    mapWidth: u16,
+    mapHeight: u16,
 };
 
-pub fn toMinecraftColors(frameData: [*c]const u8, context: *NativeMapContext) void {
-    const len = calcBufferSize(context);
+pub fn toMinecraftColors(frameData: [*c]const u8, contextNullable: ?*NativeMapContext) void {
+    if (pool == null) {
+        coreCount = std.Thread.getCpuCount() catch 1;
+        pool = PartitionPool.init(std.heap.c_allocator, coreCount) catch {
+            @panic("Failed to create color translation thread pool");
+        };
+    }
+
+    const context = contextNullable.?;
+    const len = context.mapWidth * context.mapHeight * MapByteSize;
     const dst = context.buffer[0..len];
     // do hacky pointer casting - note: we don't particularly care about endian yet
     // we handle endianness within mc.zig
@@ -39,36 +52,53 @@ pub fn toMinecraftColors(frameData: [*c]const u8, context: *NativeMapContext) vo
         // another avenue to explore is possible multi-threading our code, but as far as i remember, multithreading
         // on zig isn't exactly easy, nor does it provide exceptional performance improvements, at the cost of
         // having to rework the entire codebase to prevent race conditions, deadlocks, etc.
-        const width = context.mapWidth;
-        const linelength = width * 128;
+        const width = @intCast(usize, context.mapWidth);
 
-        {
-            const _UNROLL_LIM = 16;
-
-            // basically do the same partial loop unrolling seen in sdl-main.zig
-            var srcIndex: usize = 0;
-            while (srcIndex < len) : (srcIndex += _UNROLL_LIM) {
-                const y = srcIndex / linelength;
-                const x = srcIndex % linelength;
-
-                const mapY = y / 128;
-                const mapX = x / 128;
-
-                // it feels like we could do better than this messy math
-                const mapOffset = (mapY * width + mapX) * MapByteSize;
-                const dstIndex = mapOffset + ((y % 128) * 128) + (x % 128);
-
-                comptime var _unroll_index: usize = 0;
-                inline while (_unroll_index < _UNROLL_LIM) : (_unroll_index += 1) {
-                    const i = srcIndex + _unroll_index;
-                    const j = dstIndex + _unroll_index;
-                    dst[j] = ColorLookupTable[casted[i]];
-                }
-            }
+        const step = len / coreCount;
+        var index: usize = 0;
+        while (index < len) : (index += step) {
+            pool.?.submitTask(.{
+                casted,
+                dst,
+                index,
+                step,
+                width,
+            }) catch @panic("Failed to submit color translation task");
         }
+
+        pool.?.awaitTermination() catch |err| {
+            if (err == error.Forced) {
+                return;
+            } else {
+                @panic("awaitTermination error: not forced?");
+            }
+        };
     }
 }
 
-fn calcBufferSize(context: *NativeMapContext) usize {
-    return (context.mapWidth * context.mapHeight) * (128 * 128);
+fn processMapPartition(src: []const u16, dst: []u8, start: usize, len: usize, width: usize) void {
+    const linelength = width * 128;
+    const _UNROLL_LIM = 16;
+
+    // basically do the same partial loop unrolling seen in sdl-main.zig
+    var srcIndex: usize = start;
+    while (srcIndex < start + len) : (srcIndex += _UNROLL_LIM) {
+        const y = srcIndex / linelength;
+        const x = srcIndex % linelength;
+
+        const mapY = y / 128;
+        const mapX = x / 128;
+
+        // it feels like we could do better than this messy math
+        const mapOffset = (mapY * width + mapX) * MapByteSize;
+        const dstIndex = mapOffset + ((y % 128) * 128) + (x % 128);
+
+        comptime var _unroll_index: usize = 0;
+        inline while (_unroll_index < _UNROLL_LIM) : (_unroll_index += 1) {
+            const i = srcIndex + _unroll_index;
+            const j = dstIndex + _unroll_index;
+
+            dst[j] = ColorLookupTable[src[i]];
+        }
+    }
 }
