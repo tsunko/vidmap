717c248bd7e1148a5556393dd48b0c2e1e9c2672
tsunko <tsunko@protonmail.com>
2021-07-27 22:51:11 -0700

    Remove AVError in favor of inferred errors + remove initAVCodec

--- nativemap/src/av-common.zig
+++ nativemap/src/av-common.zig
@@ -5,26 +5,3 @@ pub usingnamespace @cImport({
     @cInclude("libswscale/swscale.h");
     @cInclude("libswresample/swresample.h");
 });
-
-pub const AVError = error{
-    FailedOpenInput,
-    FailedGuessingFormat,
-    FailedAvioOpen,
-    FailedFindStreamInfo,
-    FailedSwrContextAlloc,
-    FailedFindCodec,
-    FailedCodecOpen,
-    FailedFrameAlloc,
-    NoStreamFound,
-    FailedStreamCreation,
-    BadResponseError,
-    FailedSwrConvert,
-    FailedEncode,
-    FailedDecode,
-    FailedConversion,
-    BadResponse,
-};
-
-pub fn initAVCodec() void {
-    avcodec_register_all();
-}
