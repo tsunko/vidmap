82357fd6a84355c394c1112674bf60574927e59e
tsunko <tsunko@protonmail.com>
2026-02-07 22:40:01 -0800

    Switch implementations explicitly at comptime rather than having a runtime-looking branching statements. Also, implement morton routine for processors without BMI2...

--- nativemap/src/Lut.zig
+++ nativemap/src/Lut.zig
@@ -1,17 +1,6 @@
 const std = @import("std");
 const assert = std.debug.assert;
 
-// gather functions for mapping RGB -> Index
-// Lut.gatherBlock   (r[], g[], b[])                     void
-// Lut.gatherSingle  (r, g, b)                           void
-//
-// morton part generators:
-// Lut.mortonSimd   (r[], g[], b[])
-// Lut.mortonSingle (r, g, b)
-//
-// table shouldn't be pub
-// how to handle mapWidth? trick question, we don't, do the loop logic in Context.
-
 const TableAlignment = 64;
 var table: ?[]const u8 = null;
 
@@ -20,6 +9,7 @@ pub const BlockLen = SimdLen orelse 8;
 
 const U32Vec = @Vector(BlockLen, u32);
 const ResultVec = @Vector(BlockLen, u8);
+const IndexesVec = @Vector(BlockLen, i32);
 
 const builtin = @import("builtin");
 const haveAVX512F = builtin.cpu.has(.x86, .avx512f);
@@ -50,46 +40,16 @@ pub fn loadTable(src: []const u8, io: std.Io, alloc: std.mem.Allocator) !void {
 pub fn gatherBlock(r: *const [BlockLen]u8, g: *const [BlockLen]u8, b: *const [BlockLen]u8) ResultVec {
     var intermediate: U32Vec = undefined;
 
-    // this should compile down to the relevant branch since SimdLen is a constant at comptime
-    switch (SimdLen orelse -1) {
-        16, 8, -1 => |len| {
-            // we supported some sort of SIMD instruction - try to use it!
-            const rVec: U32Vec = mortonSimd(r[0..BlockLen].*);
-            const gVec: U32Vec = mortonSimd(g[0..BlockLen].*);
-            const bVec: U32Vec = mortonSimd(b[0..BlockLen].*);
-
-            const indexes: @Vector(BlockLen, i32) = @bitCast((rVec << @splat(2)) | (gVec << @splat(1)) | bVec);
-
-            if (len == 16 and haveAVX512F) {
-                // AVX512
-                var mask: u16 = 0xFFFF;
-                asm volatile (
-                    \\ vpgatherdd (%[base], %[indexes], 1), %[result]{%[mask]}
-                    : [result] "=&v" (intermediate),
-                      [mask] "+&{k1}" (mask),
-                    : [base] "r" (table.?.ptr),
-                      [indexes] "v" (indexes),
-                    : .{ .memory = true }
-                );
-            } else if (len == 8 and haveAVX2 and builtin.zig_backend != .stage2_x86_64) {
-                // this is apparently invalid; i have no idea what the actual correct operands are lappDumb
-                var mask: @Vector(BlockLen, i32) = @splat(@as(i32, -1));
-                asm volatile (
-                    \\ vpgatherdd %[mask], (%[base], %[indexes], 1), %[result]
-                    : [result] "=&x" (intermediate),
-                      [mask] "+&x" (mask),
-                    : [base] "r" (table.?.ptr),
-                      [indexes] "x" (indexes),
-                    : .{ .memory = true }
-                );
-            } else {
-                // scalar
-                inline for (0..BlockLen) |i| {
-                    intermediate[i] = gatherSingle(r[i], g[i], b[i]);
-                }
-            }
-        },
-        else => @compileError(std.fmt.comptimePrint("unsupported vector length {d}", .{ SimdLen })),
+    if (gatherSimdMaybe) |gatherSimd| {
+        const rVec: U32Vec = mortonSimd(r[0..BlockLen].*);
+        const gVec: U32Vec = mortonSimd(g[0..BlockLen].*);
+        const bVec: U32Vec = mortonSimd(b[0..BlockLen].*);
+        const indexes: @Vector(BlockLen, i32) = @bitCast((rVec << @splat(2)) | (gVec << @splat(1)) | bVec);
+        intermediate = gatherSimd(indexes);
+    } else {
+        inline for (0..BlockLen) |i| {
+            intermediate[i] = gatherSingle(r[i], g[i], b[i]);
+        }
     }
 
     // sanity check to see if we somehow mapped to the invalid 4 extra bytes we pad at the end
@@ -101,6 +61,40 @@ pub fn gatherBlock(r: *const [BlockLen]u8, g: *const [BlockLen]u8, b: *const [Bl
 /// Look up a single matching color based on the input RGB color.
 pub fn gatherSingle(r: u8, g: u8, b: u8) u8 {
     return table.?[mortonSingle(r) << 2 | mortonSingle(g) << 1 | mortonSingle(b)];
+}
+
+const gatherSimdMaybe: ?fn(IndexesVec) U32Vec = switch (SimdLen orelse -1) {
+    16 => gatherAVX512,
+    8 => gatherAVX2,
+    else => null
+};
+
+fn gatherAVX512(indexes: IndexesVec) U32Vec {
+    var ret: U32Vec = undefined;
+    var mask: u16 = 0xFFFF;
+    asm volatile (
+        \\ vpgatherdd (%[base], %[indexes], 1), %[result]{%[mask]}
+        : [result] "=&v" (ret),
+          [mask] "+&{k1}" (mask),
+        : [base] "r" (table.?.ptr),
+          [indexes] "v" (indexes),
+        : .{ .memory = true }
+    );
+    return ret;
+}
+
+fn gatherAVX2(indexes: IndexesVec) U32Vec {
+    var ret: U32Vec = undefined;
+    var mask: @Vector(BlockLen, i32) = @splat(@as(i32, -1));
+    asm volatile (
+        \\ vpgatherdd %[mask], (%[base], %[indexes], 1), %[result]
+        : [result] "=&x" (ret),
+          [mask] "+&x" (mask),
+        : [base] "r" (table.?.ptr),
+          [indexes] "x" (indexes),
+        : .{ .memory = true }
+    );
+    return ret;
 }
 
 fn mortonSimd(val: U32Vec) U32Vec {
@@ -122,9 +116,13 @@ fn mortonSimd(val: U32Vec) U32Vec {
     return ret;
 }
 
-// NOTE: not going to bother switching between pdep and standard masking; a processor that doesn't support BMI2 probably
-// would halt and catch fire trying to put videos on a minecraft map.
-fn mortonSingle(val: u32) u32 {
+const mortonSingle: fn(u32) u32 =
+    if (std.Target.x86.featureSetHas(builtin.cpu.features, .bmi2))
+        mortonSingleAsm
+    else
+        mortonSingleEmulated;
+
+fn mortonSingleAsm(val: u32) u32 {
     const src: u32 = (val >> 2) & 0x3F;
     const mask: u32 = 0b0010_0100_1001_0010_01;
     return asm (
@@ -133,4 +131,23 @@ fn mortonSingle(val: u32) u32 {
         : [src] "r" (src),
           [mask] "r" (mask),
     );
+}
+
+// so about that whole "if you don't have bmi2, you'd probably hcf"...
+// a server i tested this on didn't have bmi2, it didn't even have avx2...
+fn mortonSingleEmulated(val: u32) u32 {
+    var result: u32 = 0;
+    var src: u32 = (val >> 2) & 0x3F;
+    var mask: u32 = 0b0010_0100_1001_0010_01;
+    var lowest: u32 = 0;
+
+    while (mask != 0) : (mask ^= lowest){
+        lowest = mask & -% mask;
+        if (src & 1 != 0) {
+            result |= lowest;
+        }
+        src >>= 1;
+    }
+
+    return result;
 }
